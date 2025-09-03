a0ff97649d48649143f31969cdc7a523af32bd91
youko <youko@LAPTOP-5I7KSMUI>
2025-09-03 23:09:45 +0900

    課題提出しました。

---
+++ curriculumB/src/question_1_23/Animal.java
@@ -0,0 +1,36 @@
+package question_1_23;
+
+public class Animal {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+	private double length;
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	private int speed;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
---
+++ curriculumB/src/question_1_23/Main.java
@@ -0,0 +1,33 @@
+package question_1_23;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		
+		下記がコンソールに出力されるように作成してください
+		※thisとsetterとgetterとフィールドを使ってください
+		
+		動物名：ライオン
+		体長：2.1m
+		速度：80km/h
+		
+		*/
+
+		Animal lion = new Animal();
+
+		//lion.name = "ライオン";
+
+		lion.setName("ライオン");
+
+		System.out.println("動物名：" + lion.getName());
+
+		lion.setLength(2.1);
+
+		System.out.println("体長：" + lion.getLength() + "m");
+
+		lion.setSpeed(80);
+
+		System.out.println("速度：" + lion.getSpeed() + "km/h");
+	}
+}
