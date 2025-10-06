72aa972f71acfe51ff173c91d56118c9cc61dbef
youko <youko@LAPTOP-5I7KSMUI>
2025-10-06 22:51:00 +0900

    課題提出しました。

---
+++ curriculumB/src/question_1_25main/Main.java
@@ -0,0 +1,27 @@
+package question_1_25main;
+
+import java.util.Scanner;
+
+import question_1_25sub.Hero;
+
+//Main.java
+
+public class Main {
+	public static void main(String[] args) {
+		// Studentクラスのインスタンスを生成
+
+		Scanner scanner = new Scanner(System.in);
+
+		String username = scanner.nextLine();
+
+		Hero student = new Hero();
+		student.setName(username);
+		System.out.println("こんにちは 「 " + student.getName() + "  」 さん");
+
+		student.showStatus(); // Studentクラス独自のメソッド
+
+		System.out.println(); // 改行
+		student.introduce();
+
+	}
+}
