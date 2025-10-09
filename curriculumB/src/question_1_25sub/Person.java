93d70d124bf66767c2b484ed76e3fa40e8ae5d6e
youko <youko@LAPTOP-5I7KSMUI>
2025-10-09 10:48:20 +0900

    課題提出しました。

---
+++ curriculumB/src/question_1_25sub/Person.java
@@ -0,0 +1,21 @@
+package question_1_25sub;
+
+public class Person {
+	// 属性（フィールド）
+	protected String name;
+
+	// コンストラクタ
+	public Person(String name) {
+		this.name = name;
+
+	}
+
+	public Person() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	// メソッド
+	public void introduce() {
+		System.out.println("こんにちは");
+	}
+}
